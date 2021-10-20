8521392832e5ae63af336eb31233fbd1b211ce89
Jay <jay@atliq.com>
2021-10-20 18:02:40 +0530

    Added login

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.serializers import UserSerializers
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+import jwt, datetime
 
 class RegisterView(APIView):
     def post(self, request):
@@ -8,3 +11,32 @@ class RegisterView(APIView):
         serializers.is_valid(raise_exception=True)
         serializers.save()
         return Response(serializers.data)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+        return response
